9498a0a7a4aafc6a28ba0515a3843477446ff768
David Goh <david.goh@auntygrace.com.au>
2024-04-10 16:24:47 +1000

    Remove vita-versions.sh

--- images/ubuntu/assets/post-gen/vita-bin-setup-apt.sh
+++ images/ubuntu/assets/post-gen/vita-bin-setup-apt.sh
@@ -1,19 +1,50 @@
 #!/bin/bash -xe
+#
+# v--- IMPORTANT NOTE ---v
+#
+# NOTE: Changes should be made in the source of truth here:
+# https://github.com/Irys-Solutions/vita/blob/main/bin/vita-versions.sh
+#
+# After that then they should be copied to here:
+# https://github.com/Irys-Solutions/runner-images/blob/main/runner-images/images/ubuntu/assets/post-gen/vita-bin-setup-apt.sh
+# https://github.com/Irys-Solutions/runner-images/blob/main/runner-images/images/ubuntu/scripts/build/install-vita-bin-setup-apt.sh
+#
+# Assuming vita and runner-images are checked out in the same parent directory;
+# to copy to the runner-images repository, in the vita repository, run:
+#
+# make setup-apt-to-runner-images
+#
+# ^--- IMPORTANT NOTE ---^
+
+node_version="18"
+postgres_server_version="15"  # NOTE: vita-ci-cd.yml has to be updated with the same version
+python_version="3.12"
+python_source_version="${python_version}.3"
+
+export NODE_VERSION="${node_version}"
+export POSTGRES_SERVER_VERSION="${postgres_server_version}"
+export PYTHON_VERSION="${python_version}"
+export PYTHON_SOURCE_VERSION="${python_source_version}"
+#!/bin/bash -xe
 
 # v--- IMPORTANT NOTE ---v
 #
-# NOTE: Changes here should be copied to:
-# https://github.com/Irys-Solutions/vita/blob/main/bin/setup-apt.sh
+# NOTE: Changes should be made in the source of truth here:
+# https://github.com/Irys-Solutions/vita/blob/main/bin/vita-versions.sh
+#
+# After that then they should be copied to here:
 # https://github.com/Irys-Solutions/runner-images/blob/main/runner-images/images/ubuntu/assets/post-gen/vita-bin-setup-apt.sh
 # https://github.com/Irys-Solutions/runner-images/blob/main/runner-images/images/ubuntu/scripts/build/install-vita-bin-setup-apt.sh
+#
 # Assuming vita and runner-images are checked out in the same parent directory;
 # to copy to the runner-images repository, in the vita repository, run:
+#
 # make setup-apt-to-runner-images
 #
 # ^--- IMPORTANT NOTE ---^
 basedir=$(dirname "$0")
 
-source "${basedir}/vita-versions.sh"
+[ -f "${basedir}/vita-versions.sh" ] && source "${basedir}/vita-versions.sh"
 
 [ X"$(uname -s)"X = X"Linux"X ] || exit 0
 
